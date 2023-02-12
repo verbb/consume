687df65089c6bfefe544dd79289ed201a4261ab6
Josh Crawford <joshua@sgroup.com.au>
2023-02-12 20:04:26 +1100

    Fix Redirect URI not working correctly for multi-sites

--- src/base/OAuthClient.php
+++ src/base/OAuthClient.php
@@ -85,7 +85,10 @@ abstract class OAuthClient extends Client implements OAuthProviderInterface
 
     public function getRedirectUri(): ?string
     {
-        return UrlHelper::siteUrl('consume/auth/callback');
+        $siteId = Craft::$app->getSites()->getPrimarySite()->id;
+
+        // We should always use the primary site for the redirect
+        return UrlHelper::siteUrl('consume/auth/callback', null, null, $siteId);
     }
 
     public function getAuthorizationUrlOptions(): array
