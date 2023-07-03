3d837a114e631868ca6d7848ce3de9c167988086
Josh Crawford <joshua@sgroup.com.au>
2023-07-04 08:42:11 +1000

    Add `hash_str` and `hash_hmac` Twig filters to help construct auth strings in Twig templates for some providers

--- src/twigextensions/Extension.php
+++ src/twigextensions/Extension.php
@@ -6,6 +6,7 @@ use verbb\consume\Consume;
 use Craft;
 
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 class Extension extends AbstractExtension
@@ -25,8 +26,26 @@ class Extension extends AbstractExtension
         ];
     }
 
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('hash_str', [$this, 'hash_str']),
+            new TwigFilter('hash_hmac', [$this, 'hash_hmac']),
+        ];
+    }
+
     public function fetchData(array|string $client, string $method = 'GET', string $uri = '', array $options = []): mixed
     {
         return Consume::$plugin->getService()->fetchData($client, $method, $uri, $options);
+    }
+
+    public function hash_str(string $data, string $algo, bool $binary = false): string
+    {
+        return hash($algo, $data, $binary);
+    }
+
+    public function hash_hmac(string $data, string $algo, string $key, bool $binary = false): string
+    {
+        return hash_hmac($algo, $data, $key, $binary);
     }
 }
